daa4b5d9edc2c5c4c20b81f700660fe042a92d8a
Dymodeus <dymodeus@gmail.com>
2023-12-06 18:04:21 +0100

    Part one of day 3 done

--- three.py
+++ three.py
@@ -5,19 +5,36 @@ import os
 with open(os.path.join(input_path, "input3.txt"), "r") as file:
     lines = file.readlines()
 
-numbers = []
-
-for ind, line in enumerate(lines):
-    numbers.append([(int(m[0]), m.span()) for m in re.finditer(r"\d+", line)])
-
 
 def check_for_chars(x1, x2, y1, y2):
     flag = False
-    for ind1, ind2 in [range(x1, x2), range(y1, y2)]:
-        print(ind1, ind2)
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            if (not lines[y][x].isalnum()) and (lines[y][x] != "."):
+                flag = True
     return flag
 
 
-# check first row
-for ind, line in enumerate(numbers):
-    check_for_chars(1, 2, 1, 2)
+def task1():
+    numbers = []
+
+    for ind, line in enumerate(lines):
+        numbers.append([(int(m[0]), max(m.span()[0] - 1, 0), min(m.span()[1] + 1, len(line) - 1)) for m
+                        in re.finditer(r"\d+", line)])
+
+    sum_of_numbers = 0
+    # check rows
+    for ind, line in enumerate(numbers):
+        # check the numbers in the line
+        for number in line:
+            line1 = max(ind - 1, 0)
+            line2 = min(ind + 2, 140)
+            if check_for_chars(number[1], number[2], line1, line2):
+                sum_of_numbers += number[0]
+    print(f"The answer for task 1 is {sum_of_numbers}\n")
+
+
+if __name__ == "__main__":
+    task1()
+
+
