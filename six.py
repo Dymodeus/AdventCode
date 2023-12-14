fb99e59bc13f97b66dbaa9053b16b1defa52b3c9
Dymodeus <dymodeus@gmail.com>
2023-12-14 16:16:22 +0100

    Finished part 1 of day 6

--- six.py
+++ six.py
@@ -1,11 +1,12 @@
 from config import input_path
+import numpy as np
 import os
 
 
 with open(os.path.join(input_path, "input6.txt"), "r") as file:
     lines = file.readlines()
     times = lines[0].split(':')[1].split()
-    dist = lines[1].split(':')[1].split()
+    dists = lines[1].split(':')[1].split()
 
 
 def amount_of_records(dist, time):
@@ -18,9 +19,17 @@ def amount_of_records(dist, time):
 def task1():
     ans = 1
     for ind in range(len(times)):
-        ans *= amount_of_records(int(dist[ind]), int(times[ind]))[0]
+        ans *= amount_of_records(int(dists[ind]), int(times[ind]))[0]
     print(f"The answer to part one is {ans}\n")
+
+
+def task2():
+    time = int(times[0] + times[1] + times[2] + times[3])
+    dist = int(dists[0] + dists[1] + dists[2] + dists[3])
+    ans = amount_of_records(dist, time)
+    print(f"The answer to part two is {ans[0]}")
 
 
 if __name__ == "__main__":
     task1()
+    task2()
