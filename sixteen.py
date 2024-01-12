33e104a5c3b8e1d0ddff41fd2434d45aec4e177b
Dymodeus <dymodeus@gmail.com>
2024-01-12 16:29:45 +0100

    Day 16 done

--- sixteen.py
+++ sixteen.py
@@ -2,14 +2,12 @@ from config import input_path
 import os
 import numpy as np
 
-lines = np.array([list(line.rstrip()) for line in open(os.path.join(input_path, "input16.txt"), "r+")])
-directions = np.empty_like(lines)
-energized = np.empty_like(lines)
-# print([line[-1] == "\\" for line in lines])
+test = False
 
-test_line = np.array([list(line.rstrip()) for line in open(os.path.join(input_path, "test_input16.txt"), "r+")])
-test_directions = np.empty_like(test_line)
-test_energized = np.empty_like(test_line)
+if test:
+    lines = np.array([list(line.rstrip()) for line in open(os.path.join(input_path, "test_input16.txt"), "r+")])
+else:
+    lines = np.array([list(line.rstrip()) for line in open(os.path.join(input_path, "input16.txt"), "r+")])
 # print(test_line)
 
 # print([[None]*len(test_line[0])]*len(test_line))
@@ -19,60 +17,74 @@ new_direction_back = {"N": "W", "E": "S", "S": "E", "W": "N"}
 new_direction_forward = {"N": "E", "E": "N", "S": "W", "W": "S"}
 
 
-test_energized[0][0] = "#"
-test_directions[0][0] = "E"
-# print(test_directions)
-
-
 def get_energized_tiles(start_tile, start_direction):
+    directions = np.empty_like(lines)
+    energized = np.empty_like(lines)
     beams = [[start_tile[0], start_tile[1], start_direction]]  # row, column, direction
-    directions[start_tile[0]][start_tile[1]] = start_direction
-    energized[start_tile[0]][start_tile[1]] = "#"
+    # directions[start_tile[0]][start_tile[1]] = start_direction
+    # energized[start_tile[0]][start_tile[1]] = "#"
+    step = 0
     while True:
+        step += 1
+        new_beams = beams.copy()
+        to_delete = []
         for ind, beam in enumerate(beams):
             new_row = beam[0] + direction_changes[beam[2]][0]
             new_column = beam[1] + direction_changes[beam[2]][1]
             if new_row < 0 or new_column < 0 or new_row >= len(lines) or new_column >= len(lines[0]):
-                beams.pop(ind)
-                break
-            beams[ind][0], beams[ind][1] = new_row, new_column
-            energized[new_row, new_column] = "#"
-            # print(beams)
-            if lines[new_row][new_column] == "\\":
-                new_direction = new_direction_back[beam[2]]
-                beams[ind][2] = new_direction
-                if new_direction in directions[new_row, new_column]:
-                    beams.pop(ind)
-                else:
-                    directions[new_row, new_column] += new_direction
-            elif lines[new_row][new_column] == "/":
-                new_direction = new_direction_forward[beam[2]]
-                beams[ind][2] = new_direction
-                if new_direction in directions[new_row, new_column]:
-                    beams.pop(ind)
-                else:
-                    directions[new_row, new_column] += new_direction
-            elif lines[new_row][new_column] == "|" and (beam[2] == "E" or beam[2] == "W"):
-                if "S" in directions[new_row, new_column]:
-                    beams.pop(ind)
-                else:
-                    beams[ind][2] = "S"
-                    directions[new_row, new_column] += "S"
-                if "N" not in directions[new_row, new_column]:
-                    beams.append([new_row, new_column, "N"])
-                    directions[new_row, new_column] += "N"
-            elif lines[new_row][new_column] == "-" and (beam[2] == "N" or beam[2] == "S"):
-                if "E" in directions[new_row, new_column]:
-                    beams.pop(ind)
-                else:
-                    beams[ind][2] = "E"
-                    directions[new_row, new_column] += "E"
-                if "W" not in directions[new_row, new_column]:
-                    beams.append([new_row, new_column, "W"])
-                    directions[new_row, new_column] += "W"
+                # new_beams.pop(ind)
+                to_delete.append(ind)
             else:
-                directions[new_row, new_column] += beam[2]
-
+                new_beams[ind][0], new_beams[ind][1] = new_row, new_column
+                energized[new_row, new_column] = "#"
+                # print(beams)
+                if lines[new_row][new_column] == "\\":
+                    new_direction = new_direction_back[beam[2]]
+                    new_beams[ind][2] = new_direction
+                    if new_direction in directions[new_row, new_column]:
+                        # new_beams.pop(ind)
+                        to_delete.append(ind)
+                    else:
+                        directions[new_row, new_column] += new_direction
+                elif lines[new_row][new_column] == "/":
+                    new_direction = new_direction_forward[beam[2]]
+                    new_beams[ind][2] = new_direction
+                    if new_direction in directions[new_row, new_column]:
+                        # new_beams.pop(ind)
+                        to_delete.append(ind)
+                    else:
+                        directions[new_row, new_column] += new_direction
+                elif lines[new_row][new_column] == "|" and (beam[2] == "E" or beam[2] == "W"):
+                    if "S" in directions[new_row, new_column]:
+                        # new_beams.pop(ind)
+                        to_delete.append(ind)
+                    else:
+                        new_beams[ind][2] = "S"
+                        directions[new_row, new_column] += "S"
+                    if "N" not in directions[new_row, new_column]:
+                        new_beams.append([new_row, new_column, "N"])
+                        directions[new_row, new_column] += "N"
+                elif lines[new_row][new_column] == "-" and (beam[2] == "N" or beam[2] == "S"):
+                    if "E" in directions[new_row, new_column]:
+                        # new_beams.pop(ind)
+                        to_delete.append(ind)
+                    else:
+                        new_beams[ind][2] = "E"
+                        directions[new_row, new_column] += "E"
+                    if "W" not in directions[new_row, new_column]:
+                        new_beams.append([new_row, new_column, "W"])
+                        directions[new_row, new_column] += "W"
+                else:
+                    if beam[2] in directions[new_row, new_column]:
+                        to_delete.append(ind)
+                    else:
+                        directions[new_row, new_column] += beam[2]
+        for ind in to_delete[::-1]:
+            new_beams.pop(ind)
+        beams = new_beams.copy()
+        # if step % 100 == 0:
+        #     print(to_delete)
+        #     print(beams)
         if len(beams) == 0:
             break
 
@@ -80,15 +92,32 @@ def get_energized_tiles(start_tile, start_direction):
 
 
 def task1():
-    answer = get_energized_tiles([0, 0], "S")
+    answer = get_energized_tiles([2, -1], "E")
     print(f"The answer to part one is {answer}\n")
 
 
 def task2():
-    max_energized = 0
-
-    print("")
+    array_energized = []
+    for column in [-1, len(lines[0])]:
+        for row in range(len(lines)):
+            start_line = [row, column]
+            start_direction = "E" if column == -1 else "W"
+            sum_of_energized = get_energized_tiles(start_line, start_direction)
+            array_energized.append(sum_of_energized)
+            if row % 10 == 0:
+                print(f"Row {row} done.\n")
+    for row in [-1, len(lines)]:
+        for column in range(len(lines[0])):
+            start_line = [row, column]
+            start_direction = "S" if row == -1 else "N"
+            sum_of_energized = get_energized_tiles(start_line, start_direction)
+            array_energized.append(sum_of_energized)
+            if column % 10 == 0:
+                print(f"Column {column} done.\n")
+    print(array_energized)
+    print(f"The answer to part two is {max(array_energized)}")  # 8167 is too low
 
 
 if __name__ == "__main__":
-    task1()
+    # task1()
+    task2()
